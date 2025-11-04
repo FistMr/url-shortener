a19dce7e277c49191f3c29cc744d17b73cfa0d1b
Pavel Puchkov <pavel.pu4ckow@yandex.ru>
2025-11-04 14:42:13 +0300

    init commit

---
+++ src/test/java/com/puchkov/urlshortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puchkov.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
